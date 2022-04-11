9b40259e00cd03360dcdfb98acb0d13f08fe8ce9
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-04-11 14:12:43 +0430

    test: remove unused tests

--- tests/telegram-voice-player.test.ts
+++ tests/telegram-voice-player.test.ts
@@ -4,37 +4,12 @@ import '~/elements/telegram-voice-player';
 
 describe('Button with increment', async () => {
   beforeEach(async () => {
-    document.body.innerHTML = '<telegram-voice-player name="World"></telegram-voice-player>';
+    document.body.innerHTML = '<telegram-voice-player src="/audio.mp3"></telegram-voice-player>';
     await (window as unknown as IWindow).happyDOM.whenAsyncComplete();
     await requestUpdate();
   });
 
-  function getInsideButton(): HTMLElement | null | undefined {
-    return document.body.querySelector('telegram-voice-player')?.shadowRoot?.querySelector('button');
-  }
-
   function requestUpdate() {
     return document.body.querySelector('telegram-voice-player')?.requestUpdate();
   }
-
-  it('should increment the count on each click', async () => {
-    getInsideButton()?.click();
-    await requestUpdate();
-    expect(getInsideButton()?.innerText).toContain('1');
-  });
-
-  it('should show name props', () => {
-    getInsideButton();
-    expect(document.body.querySelector('telegram-voice-player')?.shadowRoot?.innerHTML).toContain('World');
-  });
-
-  it('should dispatch count event on button click', () => {
-    const spyClick = vi.fn();
-
-    document.querySelector('telegram-voice-player')?.addEventListener('count', spyClick);
-
-    getInsideButton()?.click();
-
-    expect(spyClick).toHaveBeenCalled();
-  });
 });
