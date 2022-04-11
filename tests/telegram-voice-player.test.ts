16ccbef2671cf7e24b535019908f5ada32da6d3d
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-04-11 16:47:44 +0430

    test: add test placeholder

--- tests/telegram-voice-player.test.ts
+++ tests/telegram-voice-player.test.ts
@@ -1,15 +1,8 @@
-import type { IWindow } from 'happy-dom';
-import { beforeEach, describe, it, vi, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import '~/elements/telegram-voice-player';
 
 describe('Button with increment', async () => {
-  beforeEach(async () => {
-    document.body.innerHTML = '<telegram-voice-player src="/audio.mp3"></telegram-voice-player>';
-    await (window as unknown as IWindow).happyDOM.whenAsyncComplete();
-    await requestUpdate();
+  it('test', async () => {
+    expect(true).to.be.true;
   });
-
-  function requestUpdate() {
-    return document.body.querySelector('telegram-voice-player')?.requestUpdate();
-  }
 });
