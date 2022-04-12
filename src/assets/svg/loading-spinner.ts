45c999a93344fed94c1112f335d42de89c7545f7
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-04-12 05:33:32 +0430

    feat: add loading

---
+++ src/assets/svg/loading-spinner.ts
@@ -0,0 +1,7 @@
+import { html } from 'lit';
+
+export default html`
+  <svg viewBox="25 25 50 50" class="loading-icon">
+    <circle stroke="currentColor" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10" stroke-width="4"></circle>
+  </svg>
+`;
--- src/elements/telegram-voice-player.ts
+++ src/elements/telegram-voice-player.ts
@@ -4,6 +4,7 @@ import { durationToTime } from '~/utils/time';
 import { filterData, normalizeData } from '~/utils/audio';
 import playSvg from '~/assets/svg/play';
 import pauseSvg from '~/assets/svg/pause';
+import loadingSpinnerSvg from '~/assets/svg/loading-spinner';
 
 /**
  * An example element.
@@ -49,13 +50,37 @@ export class TelegramVoicePlayer extends LitElement {
       margin-right: 15px;
       border: none;
       cursor: pointer;
-      padding: 15px;
       color: #fff;
+      padding: 0;
+    }
+
+    #play:disabled {
+      cursor: default;
+    }
+
+    #play > svg {
+      width: 15px;
+      height: 15px;
     }
 
     #play > .play-icon {
       position: relative;
       right: -1px;
+    }
+
+    #play > .loading-icon {
+      width: 40px;
+      height: 40px;
+      transition: all 0.3s ease 0s;
+      transform-origin: center center;
+      animation: rotate 1.5s linear infinite;
+    }
+
+    #play > .loading-icon > circle {
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+      stroke-linecap: round;
+      animation: dash 1s ease-in-out infinite;
     }
 
     #details {
@@ -82,6 +107,30 @@ export class TelegramVoicePlayer extends LitElement {
       display: flex;
       justify-content: space-between;
     }
+
+    @keyframes dash {
+      0% {
+        stroke-dasharray: 1, 200;
+        stroke-dashoffset: 0;
+      }
+
+      50% {
+        stroke-dasharray: 89, 200;
+        stroke-dashoffset: -35;
+      }
+
+      100% {
+        stroke-dasharray: 89, 200;
+        stroke-dashoffset: -124;
+      }
+    }
+
+    @keyframes rotate {
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
   `;
 
   @property({ type: String }) src = '';
@@ -95,7 +144,9 @@ export class TelegramVoicePlayer extends LitElement {
   override render() {
     return html`
       <div id="container">
-        <button id="play" @click=${this._playOrPause}>${this.isPlaying ? pauseSvg : playSvg}</button>
+        <button id="play" @click=${this._playOrPause} ?disabled=${!this.isLoaded}>
+          ${this.isPending ? loadingSpinnerSvg : this.isPlaying ? pauseSvg : playSvg}
+        </button>
         <div id="details">
           <canvas id="canvas"></canvas>
           ${this.isLoaded
@@ -112,31 +163,19 @@ export class TelegramVoicePlayer extends LitElement {
   override connectedCallback() {
     super.connectedCallback();
 
-    this._loadAudio(this.src);
+    this._loadAudio();
   }
 
-  private _loadAudio(url: string) {
+  private _loadAudio() {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioContext = new AudioContext();
-    fetch(url)
+    fetch(this.src)
       .then((response) => response.arrayBuffer())
       .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
       .then((audioBuffer) => this._drawAudioBars(audioBuffer))
-      .then(() => (this.isLoaded = true))
-      .finally(() => (this.isPending = false));
-
-    this.audio = new Audio();
-    this.audio.src = url;
-    this.audio.addEventListener('timeupdate', () => {
-      this.currentTime = Math.floor((this.audio.currentTime / this.audio.duration) * 100);
-      this._drawAudioProgress(this.currentTime);
-    });
-
-    this.audio.addEventListener('loadedmetadata', () => {
-      this.totalTime = this.audio.duration;
-    });
+      .then(() => this._setupAudio());
   }
 
   private async _drawAudioBars(audioBuffer: AudioBuffer) {
@@ -207,6 +246,23 @@ export class TelegramVoicePlayer extends LitElement {
 
     ctx.fillStyle = '#5562ff';
     ctx.fillRect(0, height / -2, (width * progress) / 100, height);
+  }
+
+  private _setupAudio() {
+    this.audio = new Audio();
+    this.audio.src = this.src;
+
+    this.audio.addEventListener('timeupdate', () => {
+      this.currentTime = Math.floor((this.audio.currentTime / this.audio.duration) * 100);
+      this._drawAudioProgress(this.currentTime);
+    });
+
+    this.audio.addEventListener('loadedmetadata', () => {
+      this.totalTime = this.audio.duration;
+      this.currentTime = 0;
+      this.isLoaded = true;
+      this.isPending = false;
+    });
   }
 
   private _playOrPause() {
