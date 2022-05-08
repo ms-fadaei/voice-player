18f61f099f59f11bda9daace2f4676289992a100
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-05-08 09:20:38 +0430

    feat: use code spliting

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import '~/elements/telegram-voice-player';
-import '~/elements/play-wave';
+export const initTelegramVoicePlayer = () => import('~/elements/telegram-voice-player');
+export const initPlayWave = () => import('~/elements/play-wave');
