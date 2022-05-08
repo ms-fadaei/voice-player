b61811d71ab08c0a211485c85d802a937f53c569
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-05-08 12:13:44 +0430

    chore: config for deploy

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,10 +1,14 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import viteConfig from '../vite.config';
 
 export default defineConfig({
-  ...viteConfig,
   root: resolve(__dirname, './'),
   publicDir: resolve(__dirname, './public'),
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './src'),
+      '@': resolve(__dirname, './playground'),
+    },
+  },
   base: 'https://ms-fadaei.github.io/voice-player/',
 });
